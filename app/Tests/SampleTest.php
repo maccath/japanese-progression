8750cdc444aeef7edb0c076dbf78ad7939683075
Katy Ereira <katy@skillshare.com>
2023-11-20 17:35:54 +0000

    chore(tests): add sample test

---
+++ app/Tests/SampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class SampleTest extends TestCase
+{
+    public function testTrue(): void
+    {
+        $this->assertTrue(true);
+    }
+}
